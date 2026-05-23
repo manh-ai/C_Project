8a6a652b6792216487753d1977ccce1bec005780
manh-ai <sonofthegod07@gmail.com>
2026-05-22 20:26:06 -0700

    Add files via upload

---
+++ intialize_an_array_with_user_input.c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int numbers [5] = {0};
+for(int i = 0; i<5 ;i++){
+    printf("enter elements: ");
+    scanf("%d", &numbers[i]);
+}
+for(int i = 0; i<5; i++){
+    printf(" %d",numbers[i]);
+}
+    return 0;
+}
